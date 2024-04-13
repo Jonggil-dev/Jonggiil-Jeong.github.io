e58cabcf20491633581c7293cd3c193806361a0e
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-04-13 18:05:36 +0900

    Docs : project 수정

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,39 +6,41 @@ const Intro = () => {
       id="Home"
       className="pt-[100px] bg-[#cba36f] h-auto flex flex-col items-center justify-center space-y-6 py-10"
     >
-      <h1 className="text-5xl font-extrabold text-[#f5f5f5] drop-shadow-lg mb-6">
-        안녕하세요, 백엔드 개발자 정종길입니다.
-      </h1>
-      <div className="flex flex-col items-start ms-[150px]">
-        <div className="mt-4">
-          <h2 className="text-3xl font-bold text-[#f5f5f5] mb-4">
-            끈기를 바탕으로 도전하는 것에 익숙합니다.
-          </h2>
-          <p className="text-xl mt-2">
-            '스마트 스쿨존' 아이디어로 공모전에서 산업통상자원부 장관상을
-            수상했습니다.
-          </p>
-          <p className="text-xl mt-2">
-            4번의 도전 끝에 이루어낸 전국 1등 경험에서 도전이 주는 즐거움을
-            깨닫게 되었습니다.
-          </p>
-        </div>
+      <div className="intro">
+        <h1 className="text-4xl font-extrabold text-[#f5f5f5] drop-shadow-lg mb-6 introUpper">
+          안녕하세요, 백엔드 개발자 정종길입니다.
+        </h1>
+        <div className="flex flex-col items-start introLower">
+          <div className="mt-4">
+            <h2 className="text-2xl font-bold text-[#f5f5f5] mb-4">
+              끈기를 바탕으로 도전을 두려워 하지 않습니다.
+            </h2>
+            <p className="text-base mt-2">
+              '스마트 스쿨존' 아이디어로 공모전에서 산업통상자원부 장관상을
+              수상했습니다.
+            </p>
+            <p className="text-base">
+              4번의 도전 끝에 이루어낸 전국 1등 경험에서 도전이 주는 즐거움을
+              깨닫게 되었습니다.
+            </p>
+          </div>
 
-        <div className="mt-10">
-          <h2 className="text-3xl font-bold text-[#f5f5f5] mb-4">
-            정답을 넘어 깊은 이해를 추구합니다.
-          </h2>
-          <p className="text-xl mt-2">
-            CI/CD Pipeline을 구축하여 자동화 배포 서비스를 제공했습니다.
-          </p>
-          <p className="text-xl mt-2">
-            단순히 자동화 배포를 구현한 것이 아닌, 프로젝트의 아키텍처 내에서 각
-            서버가 상호 작용하는 과정을 이해하였습니다.
-          </p>
-          <p className="text-xl mt-2">
-            EC2 인스턴스와 Docker 컨테이너, 리버스 프록시 서버간의 상호작용을
-            이해하고 리소스 낭비를 최소화 하였습니다.
-          </p>
+          <div className="mt-10">
+            <h2 className="text-2xl font-bold text-[#f5f5f5] mb-4">
+              정답을 넘어 깊은 이해를 추구합니다.
+            </h2>
+            <p className="text-base">
+              CI/CD Pipeline을 구축하여 자동화 배포 서비스를 제공했습니다.
+            </p>
+            <p className="text-base">
+              단순히 자동화 배포를 구현한 것이 아닌, 프로젝트의 아키텍처 내에서
+              각 서버가 상호 작용하는 과정을 이해하였습니다.
+            </p>
+            <p className="text-base">
+              EC2 인스턴스와 Docker 컨테이너, 리버스 프록시 서버간의 상호작용을
+              이해하고 리소스 낭비를 최소화 하였습니다.
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,40 @@
 import React from "react";
+import simcheongeMain from "../assets/images/simcheongMain.jpg";
+import blink from "../assets/images/blink.jpg";
 
 const Projects = () => {
   return (
-    <section id="project" className="p-12">
-      <div className="max-w-5xl mx-auto px-4">
+    <section id="project" className="min-h-screen p-12">
+      <div className="w-full mx-auto">
         <h2 className="text-4xl font-bold text-center mb-10">PROJECTS</h2>
-        <div className="flex flex-wrap justify-center items-stretch gap-8">
+        <div className="flex flex-col items-stretch gap-8">
           <ProjectItem
             title="심청이"
             period="2024.02.19(월) ~ 2024.04.05(금)"
-            position="백엔드 개발"
+            position="인프라, 백엔드 개발"
             description="청년 정책 정보를 쉽고 간편하게 제공하는 모바일 어플리케이션"
             roles={[
-              "CI/CD Pipeline 구축을 통한 자동화 배포",
-              "Spring Security + JJWT를 활용한 인증/인가 관리",
-              "크롤링과 Chatgpt API를 통한 뉴스 기사 요약",
+              {
+                title: "CI/CD Pipeline 구축을 통한 자동화 배포",
+                description: `Jenkins CI/CD 파이프라인을 통해 코드 커밋이 발생할 때마다 성공적으로 이루어진 빌드만을 프로덕션 서버에 배포했습니다.
+                EC2 인스턴스 내에서 Nginx를 리버스 프록시로 사용하고 Docker Container를 활용해 EC2내 각 서버들을 연결했습니다.
+                민감 데이터의 경우 env 파일로 Jenkins의 Credential 저장소에서 관리하고  빌드 직전에 프로젝트로 주입함으로써 보안을 강화했습니다
+                Docker 이미지의 경우 멀티 스테이지 빌드를 적용하여 불필요한 의존성과 파일을 제거하여 배포 효율성을 극대화했습니다.
+                Flutter의 APK 파일의 경우, Nginx를 통해 사용자가 특정 URL로 요청 시 최신 버전을 서빙되도록 설정했습니다.
+                또한, 개발자가 APK의 버전 관리를 할 수 있도록 별도로 볼륨 마운트 폴더를 생성했습니다`,
+              },
+              {
+                title: "Spring Security + JJWT를 활용한 인증/인가 관리",
+                description: `Spring Security와 JJWT를 사용하여 인증 및 인가 시스템을 구축했습니다. 
+                  Security의 CustomUserDetail을 통해 User의 LoginID를 기준으로 UserDetail 객체를 만들어 로그인을 진행하였습니다.
+                  또한, Security의 filterchain내 JwtAuthenticationFilter를 생성하고 UsernamePasswordAuthenticationFilter전에 위치시켜 JWT토큰을 통한 인증 시스템을 구축했습니다.`,
+              },
+              {
+                title: "크롤링과 Chatgpt API를 통한 뉴스 기사 요약",
+                description: `사용자에게 헤드라인 뉴스 기사의 요약본과 원문을 제공했습니다.
+                  JSOAP를 사용해 네이버의 최신 해드라인 기사 뉴스의 링크를 먼저 크롤링하고, 해당 링크의 기사 내용을 다시 크롤링했습니다.
+                  이렇게 추출된 뉴스 기사를 Chatgpt Api를 사용해 기사를 요약해 사용자에게 제공했습니다.`,
+              },
             ]}
             techStack={[
               "AWS EC2",
@@ -29,18 +49,35 @@ const Projects = () => {
               "Redis",
             ]}
             link="https://github.com/Jonggil-dev/Simcheonge"
+            image={simcheongeMain}
           />
-
           <ProjectItem
             title="눈 깜빡할 새"
             period="2024.01.08(월) ~ 2024.02.16(금)"
             position="프론트엔드 개발"
             description="WebRTC기술을 활용한 온라인 눈(eye) 싸움 게임"
             roles={[
-              "디자인 (피그마 UI/UX 설계)",
-              "React Context API를 통한 유저 정보 관리",
-              "Axios Interceptor를 활용한 인증/인가 요청(Jwt 토큰 관리)",
-              "WebSocket을 통한 실시간 통신 구현",
+              {
+                title: "디자인 (피그마 UI/UX 설계)",
+                description:
+                  "피그마를 사용하여 사용자 친화적인 UI/UX를 설계하고 프로토타이핑을 수행했습니다.",
+              },
+              {
+                title: "React Context API를 통한 유저 정보 관리",
+                description:
+                  "Context API를 활용하여 애플리케이션의 상태 관리를 단순화하고, 유저 정보를 전역적으로 관리했습니다.",
+              },
+              {
+                title:
+                  "Axios Interceptor를 활용한 인증/인가 요청(Jwt 토큰 관리)",
+                description:
+                  "Axios Interceptor를 통해 요청 및 응답을 중간에 가로채어 JWT 토큰을 효과적으로 관리하였습니다.",
+              },
+              {
+                title: "WebSocket을 통한 실시간 통신 구현",
+                description:
+                  "WebSocket 프로토콜을 활용하여 실시간 게임 플레이와 통신이 가능하도록 구현했습니다.",
+              },
             ]}
             techStack={[
               "Node.js",
@@ -53,9 +90,10 @@ const Projects = () => {
               "Docker",
             ]}
             link="https://github.com/Jonggil-dev/Staring-Contest"
+            image={blink}
           />
 
-          <ProjectItem
+          {/* <ProjectItem
             title="수길이네 금융마을"
             period="2023.11.16(목) ~ 2023.11.23(목)"
             position="풀스택 개발"
@@ -68,7 +106,8 @@ const Projects = () => {
             ]}
             techStack={["Vue.js", "Django", "SQLite3"]}
             link="https://github.com/Jonggil-dev/finalpjt_semester1"
-          />
+            image=""
+          /> */}
         </div>
       </div>
     </section>
@@ -83,42 +122,47 @@ function ProjectItem({
   roles,
   techStack,
   link,
+  image,
 }) {
   return (
-    <div className="max-w-md w-full bg-white shadow-lg rounded-lg transition duration-300 ease-in-out hover:scale-105 hover:shadow-xln">
-      <div className="p-5">
-        <h3 className="text-xl font-semibold mb-3">{title}</h3>
-        <div className="text-gray-800">
-          <p className="font-medium mb-2">1. 기간:</p>
-          <p className="ml-4">· {period}</p>
-          <p className="font-medium mt-4 mb-2">2. 개요:</p>
-          <p className="ml-4">· {description}</p>
-
+    <div className="w-full flex bg-white shadow-2xl rounded-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
+      <div className="w-1/3">
+        <img
+          src={image}
+          alt={`${title}`}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-2/3 p-10 flex flex-col justify-between">
+        <div>
+          <h3 className="text-xl font-semibold mb-3">{title}</h3>
+          <p className="font-medium mb-2">1. 기간: {period}</p>
+          <p className="font-medium mt-4 mb-2">2. 개요: {description}</p>
           <p className="font-medium mt-4 mb-2">3. 역할: {position}</p>
           {roles.map((role, index) => (
-            <p key={index} className="ml-4 mb-1">
-              · {role}
-            </p>
+            <div key={index} className="mb-2 ms-4">
+              <p className="font-semibold mb-1">· {role.title}</p>
+              <p className="text-gray-600 text-sm ms-3"> {role.description}</p>
+            </div>
           ))}
-
-          <p className="font-medium mt-4 mb-2">4. 기술스택:</p>
-          <div className="flex flex-wrap gap-2 mt-2 ml-4">
-            {techStack.map((tech, index) => (
-              <span
-                key={index}
-                className="px-3 py-1 bg-blue-100 text-blue-800 rounded-full text-sm"
-              >
-                {tech}
-              </span>
-            ))}
-          </div>
-
-          <p className="font-medium mt-4 mb-2">5. 링크:</p>
+        </div>
+        <p className="font-medium mt-4 mb-2">4. 기술스택:</p>
+        <div className="flex flex-wrap gap-2 mt-2">
+          {techStack.map((tech, index) => (
+            <span
+              key={index}
+              className="px-3 py-1 bg-blue-100 text-blue-800 rounded-full text-sm"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+        <div className="mt-5">
           <a
             href={link}
-            className="ml-4 text-blue-500 hover:text-blue-600 underline"
+            className="text-blue-500 hover:text-blue-600 underline"
           >
-            프로젝트 보러가기
+            ▶ 프로젝트 보러가기
           </a>
         </div>
       </div>
